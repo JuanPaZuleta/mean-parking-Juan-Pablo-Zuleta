3d1a93937ff27e55f3d5b42e7669bd95f2d7781e
JuanPaZuleta <58981808+JuanPaZuleta@users.noreply.github.com>
2020-11-19 17:00:37 -0300

    Creacion de Portafolio stack MEAN

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { mongoose } = require('./database');
+
+// configuraciones de servidores : Settings
+app.set('port', process.env.PORT || 3000); //configuracion de servidor.
+// funciones para procesar datos : Middlewares
+app.use(morgan('dev'));
+app.use(express.json()); //traductor a Json por medio de express, conocido como 'bodyParser'
+
+//rutas de nuestro servidor : Routes
+app.use(require('./routes/cars.routes'));
+
+
+//Inicio del servidor : Starting the Server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
