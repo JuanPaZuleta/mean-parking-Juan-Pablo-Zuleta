ee4780f342c02f472597ee60f93aa930e3fb8536
JuanPaZuleta <58981808+JuanPaZuleta@users.noreply.github.com>
2020-11-19 23:31:40 -0300

    Finalización metodos CRUD para los autos.

--- Server/controllers/car.controller.js
+++ Server/controllers/car.controller.js
@@ -5,31 +5,39 @@ const carCtrl = {};
 
 carCtrl.getCars = async(req, res) => {
     const cars = await car.find()
-
     res.json(cars);
 }
 
-
-carCtrl.createCar = function() {
-
+carCtrl.createCar = async(req, res) => {
+    const carr = new car(req.body);
+    await carr.save();
+    res.json({ 'status': 'Auto ingresado.' });
 }
 
-
-carCtrl.getCarById = (req, res) => {
-    res.json({
-        status: 'Aqui se van a buscar autos por Id'
-    });
+carCtrl.getCarById = async(req, res) => {
+    const carr = await car.findById(req.params.id);
+    res.json(carr);
 }
 
-carCtrl.updateCar = (req, res) => {
-    res.json({
-        status: 'Aqui se van a actualizar autos por Id'
-    });
+carCtrl.updateCar = async(req, res) => {
+    const { id } = req.params;
+    const carr = {
+        marca: req.body.marca,
+        anio: req.body.anio,
+        modelo: req.body.modelo,
+        propietario: req.body.propietario,
+        estado: req.body.estado,
+        deuda: req.body.deuda
+    }
+    await car.findByIdAndUpdate(id, { $set: carr }, { new: true });
+    res.json({ status: 'Auto modificado' });
+    //res.json({ status: 'Auto modificado' }, car.findById(req.params.id));
 }
 
-carCtrl.deleteCar = (req, res) => {
+carCtrl.deleteCar = async(req, res) => {
+    await car.findByIdAndRemove(req.params.id);
     res.json({
-        status: 'Aqui se van a Eliminar autos por Id'
+        status: 'Auto eliminado'
     });
 }
 
